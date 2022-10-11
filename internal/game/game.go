fd908ef15325dc602d682c83417c3ba2b7c069ff
Yevgeniy Zaichenko <99prblmsaint1@gmail.com>
2022-10-11 23:24:27 +0300

    Mate

--- internal/game/game.go
+++ internal/game/game.go
@@ -41,27 +41,18 @@ func (g *Game) Move(color int, move Move) bool {
 	return false
 }
 
-func (g *Game) isCheck(color int) bool {
+func (g *Game) isCheck(board [8][8]Piece,color int) bool {
 
 	// Получить позицию короля
-	var kingPosition Position
-	for v, line := range g.board {
-		for h, piece := range line {
-			if (piece.name == "king") {
-				if (piece.color == color) {
-					kingPosition = Position{H:h, V:v}
-				}
-			}
-		}
-	}
+	kingPosition := g.getKingPosition(color)
 
 	// Проверяем угражает ли чужая фигура королю
     // Перебираем все элементы борда
-	for v, line := range g.board {
+	for v, line := range board {
 		for h, piece := range line {
 			if (!piece.isEmpty) {
 				if (piece.color != color) {
-					positions := piece.GetSteps(g.board, v, h)
+					positions := piece.GetSteps(board, v, h)
 
 					for _,position := range positions {
 						if (position.H == kingPosition.H && position.V == kingPosition.V) {
@@ -81,16 +72,7 @@ func (g *Game) isCheck(color int) bool {
 func (g *Game) getCheckPiece(color int) Position {
 
 	// Получить позицию короля
-	var kingPosition Position
-	for v, line := range g.board {
-		for h, piece := range line {
-			if (piece.name == "king") {
-				if (piece.color == color) {
-					kingPosition = Position{H:h, V:v}
-				}
-			}
-		}
-	}
+	kingPosition := g.getKingPosition(color)
 
 	// Проверяем угражает ли чужая фигура королю
     // Перебираем все элементы борда
@@ -110,4 +92,54 @@ func (g *Game) getCheckPiece(color int) Position {
 	}
 
 	return Position{H:0,V:0}
+}
+
+func (g *Game) getKingPosition(color) Position {
+	var kingPosition Position
+	for v, line := range g.board {
+		for h, piece := range line {
+			if (piece.name == "king") {
+				if (piece.color == color) {
+					kingPosition = Position{H:h, V:v}
+				}
+			}
+		}
+	}
+	return kingPosition
+}
+
+
+// Перебираем все наши фигуры, получаем все ходы каждой фигуры, и делаем ход
+// Если после хода это не шах, значит это не мат
+// Если все равно шах, то откатываем его и пробуем дальше
+// Можно оптимизирвать
+// И сначала проверять дает ли шах та фигура которая до этого давала, и только после этого проверять всю доску шах
+func (g *Game) isMate() bool {
+
+	board := copy(g.board)
+
+	for v, line := range g.board {
+		for h, piece := range line {
+			if (!piece.isEmpty) {
+				if (piece.color == color) {
+					positions := piece.GetSteps(g.board, v, h)
+					for _,position := range positions {
+						
+						tmp := board[position.V][position.H]
+						board[position.V][position.H] = board[v][h]
+						board[v][h] = NewPiece(0, "empty")
+
+						if (g.isCheck(board, color)) {
+							board[v][h] = board[position.V][position.H]
+							board[position.V][position.H] = tmp
+						} else {
+							return false
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return true
 }
